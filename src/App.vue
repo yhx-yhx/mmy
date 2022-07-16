d28535d5c3b23e224fdbcceeb956ff1b2a005eaa
yhx <18747682696@163.com>
2022-07-16 18:41:10 +0800

    vue-element-pc

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <login></login>
+  </div>
+</template>
+<script>
+import login from "@/views/login/index.vue";
+export default {
+  components: { login },
+};
+</script>
+<style >
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
+
---
+++ src/views/login/index.vue
@@ -0,0 +1,98 @@
+<template >
+  <div class="body" ref="body">
+    <div class="login-from">
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="用户名" prop="checkPass">
+          <el-input
+            type="username"
+            v-model="ruleForm.username"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="pass">
+          <el-input
+            type="password"
+            v-model="ruleForm.pass"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >提交</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    var validateusername = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        username: "",
+        pass: "",
+      },
+      rules: {
+        username: [{ validator: validateusername, trigger: "blur" }],
+        pass: [{ validator: validatePass, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
+
+<style scoped >
+.login-from {
+  width: 450px;
+  margin: 200px auto;
+}
+.body {
+  background-image: url("../../assets/common/login.jpg");
+  background-position: center;
+
+  min-height: 100%;
+  width: 100%;
+}
+</style>
